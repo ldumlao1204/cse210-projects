51822d8c0e0d82bbb24f6a4ff314cf258ffcb04b
ldumlao1204 <leilany.dumlao@gmail.com>
2025-11-27 19:22:58 +0800

    Inheritance Assignment

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathassignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathassignment.GetSummary());
+        Console.WriteLine(mathassignment.GetHomeworkList());
+
+        WritingAssignment writingassignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingassignment.GetSummary());
+        Console.WriteLine(writingassignment.GetWritingInformation());
     }
 }
